8e884c2cb06cc224541920c5dda4bf198075615e
Matt Ruehle <matthew.t.ruehle@gmail.com>
2015-11-16 19:58:39 -0500

    faster scanning

--- code_scanner.py
+++ code_scanner.py
@@ -2,12 +2,56 @@
 Basic QR code-reading for the raspberry pi. Uses zbarlight, openCV, and the Image class.
 Note: This is a work in progress, -m@.
 """
-
 from PIL import Image
 import io
 # from picamera.array import PiRGBArray
 import picamera
 # import cv2
+
+
+# import fileinput
+# import math
+
+# def specific_permutation(m, n):
+#     '''gets mth and nth permutation fron an input'''
+#     total_number_of_options = math.factorial(n);
+#     current_possible_integers = [i for i in range(n)]
+#     output_list = []
+#     for i in range(n):
+#         f = math.factorial(n - (i+1))
+#         print m, f
+#         which_tile = m / f
+#         print which_tile, current_possible_integers[which_tile-1]
+#         output_list.append(current_possible_integers[which_tile-1])
+#         current_possible_integers.pop(which_tile)
+#         m = m%f
+#     for i in output_list:
+#         print output_list[i], ' ',
+#     print '\n'       
+        
+        
+# if __name__ == "__main__":
+#     # for line in fileinput.input():
+#     #     two_strings = line.split(' ')
+#     #     m = int(two_strings[0])
+#     #     n = int(two_strings[1])
+#     m = 719
+#     n = 6
+#     specific_permutation(m, n)
+        
+
+
+
+
+
+
+
+
+
+
+
+# '''
+# #=============================
 import zbarlight
 import time
 
@@ -38,7 +82,7 @@ class QRDetector(object):
 		"""
 		Looks through camera once; if there's a QR code, returns it. Otherwise, returns -1.
 		"""
-		self.cam.capture(self.stream, format="jpeg")
+		self.cam.capture(self.stream, format="jpeg", use_video_port=True)
 		self.stream.truncate()
 		self.stream.seek(0)
 		img = Image.open(self.stream)
@@ -58,4 +102,5 @@ class QRDetector(object):
 
 if __name__ == "__main__":
 	qrd = QRDetector()
+	
 	qrd.run_once()
