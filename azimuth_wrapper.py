54ce58c8114c2209bd956227d032414a6e6525d3
buttegab <buttegab@users.noreply.github.com>
2015-11-17 12:06:39 -0500

    Update azimuth_wrapper.py
    Moved the speech recognition call into the main loop instead of init

--- azimuth_wrapper.py
+++ azimuth_wrapper.py
@@ -11,13 +11,10 @@ class FetchBot(object):
 	def __init__(self, serial_port="/dev/ttyACM0"):
 		self.qrd = qr_detector.QRDetector()
 		self.ser = serial.Serial(serial_port, 9600, timeout=2)
-		self.target_queue = [1,3,2,4]
+		#self.target_queue = [1,3,2,4]
 		self.hasObject = False
-		recognizedCommands = ["salt", "pepper", "one", "two", "three"]
-		if speechTester.word_list in reconizedCommands:
-			self.set_target(speechTester.word_list) #needs to be changed once speech_testing is set up like a class
-		else:
-			print "Unrecognized Command"
+		self.recognizedCommands = ["salt", "pepper", "one", "two", "three", "azimuth"]
+
 	def check_if_at_target(self):
 		if len(self.target_queue) == 0:
 			return
@@ -27,6 +24,7 @@ class FetchBot(object):
 			self.stop_robot()
 			self.do_something()
 			self.target_queue.pop(0)
+			
 	def grab(self):
 		GRAB_COMMAND = "G"
 		ser.write(GRAB_COMMAND)
@@ -36,12 +34,17 @@ class FetchBot(object):
 		ser.write(STOP_COMMAND)
 
 	def mainloop(self):
+		if speechTester.word_list in self.reconizedCommands: #needs to be changed to whatever is actually the call for word list
+			self.set_target(speechTester.word_list) #needs to be changed once speech_testing is set up like a class
+		else:
+			print "Unrecognized Command"
 		self.check_if_at_target()
+		
 
 	def do_something(self):
 		TURN_COMMAND = "<" #sets the command to pickup the object
 		ser.write(TURN_COMMAND)
-		return
+		
 	
 	def set_target(self, new_target):
 		self.target_queue.extend(new_target)
@@ -50,4 +53,4 @@ class FetchBot(object):
 		self.target_queue = []
 
 if __name__ == "__main__":
-	FetchBot.mainloop()
+	FetchBot().mainloop()
