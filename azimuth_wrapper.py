4bd660b9038eb94cfa379f4ed4aa51ef473806d4
Matt Ruehle <matthew.t.ruehle@gmail.com>
2015-12-01 11:24:19 -0500

    Adding the if-checks to the wrapper's main loop function.

--- azimuth_wrapper.py
+++ azimuth_wrapper.py
@@ -6,7 +6,6 @@ TODO: probably, add some try-catches.
 """
 import serial
 import qr_detector
-# import collections
 import speech_handler #needs to be set up correctly
 
 class FetchBot(object):
@@ -21,32 +20,40 @@ class FetchBot(object):
 
 	def check_if_at_target(self):
 		if len(self.target_queue) == 0:
+			STOP_COMMAND = "S"
+			self.ser.write(STOP_COMMAND)
 			return
-		qr_val = "asdf"
+		qr_val = "bluh"
 		qr_val = self.qrd.scan()
 		if str(qr_val) == str(self.target_queue[0]):
-			print "arrived at ", self.target_queue[0]
-			self.stop_robot()
-			self.do_something() # also needs to make the robot start moving again.
+			t = self.target_queue[0]
+			print "arrived at ", t
+			if t in ["1", "2", "3"]:
+				if self.hasObject:
+					DROP_COMMAND = "D" # TODO ofc.
+					self.ser.write(DROP_COMMAND)
+					self.hasObject = False
+					time.sleep(1)
+					TURN_COMMAND = "<"
+					self.ser.write(TURN_COMMAND)
+					time.sleep(1)
+			elif t in ["salt", "pepper"]:
+				if not self.hasObject:
+					GRAB_COMMAND = "G"
+					self.ser.write(GRAB_COMMAND)
+					time.sleep(1) # lets it finish
+					self.hasObject = True
+					TURN_COMMAND = "<"
+					self.ser.write(TURN_COMMAND)
+					time.sleep(1)
+			FWD_COMMAND = "F"
+			self.ser.write(FWD_COMMAND)
 			self.target_queue.pop(0)
 			
-	def grab(self):
-		GRAB_COMMAND = "G"
-		self.ser.write(GRAB_COMMAND)
-		
-	def stop_robot(self):
-		STOP_COMMAND = "S"
-		self.ser.write(STOP_COMMAND)
-
 	def mainloop(self): #First checks the list of found voice commands and compares them to what it has heard. If there is a match and it isn't "azimuth", it appends the command list. If it is "azimuth" it recognizes its own name. Else, it returns a false command
 		self.check_if_at_target()
 		#should probably also have it move/go, if it doesn't default to moving forward.
 
-	def do_something(self):
-		TURN_COMMAND = "<" #sets the command to pickup the object
-		self.ser.write(TURN_COMMAND)
-		
-	
 	def set_target(self, new_targets):
 		for i in new_targets:
 			i = str(i)
