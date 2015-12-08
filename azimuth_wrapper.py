28f1f2c88eccb5caa10a2c6f51bedf467f550007
Matt Ruehle <matthew.t.ruehle@gmail.com>
2015-12-08 00:41:26 -0500

    Threading, hopefully!

--- azimuth_wrapper.py
+++ azimuth_wrapper.py
@@ -4,6 +4,7 @@ TODO: probably, add some try-catches.
 import serial
 import qr_detector
 import speech_handler #needs to be set up correctly
+import threading
 
 class FetchBot(object):
 
@@ -15,13 +16,15 @@ class FetchBot(object):
 		self.hasObject = False
 		self.words_to_targets_dict = {"one": "1", "1": "1", "won": "1", "2":"2", "3":"3", "two": "2", "to": "2", "too": "2", "three": "3", "tree": "3", "salt": "salt", "pepper": "pepper", "rapper":"pepper"} # Do we want these strings as the values encoded by the QR codes? Or are we going for coordinates?
 		self.sr = speech_handler.Loop_speech_handler(self.set_target)
+		self.main_loops_so_far = 0 # for debugging.
+
 
 	def check_if_at_target(self):
 		if len(self.target_queue) == 0:
 			STOP_COMMAND = "S"
 			self.ser.write(STOP_COMMAND)
 			return
-		qr_val = "bluh"
+		qr_val = ""
 		qr_val = self.qrd.scan()
 		if str(qr_val) == str(self.target_queue[0]):
 			t = self.target_queue[0]
@@ -58,9 +61,14 @@ class FetchBot(object):
 			self.ser.write(FWD_COMMAND)
 			self.target_queue.pop(0)
 			
-	def mainloop(self): #First checks the list of found voice commands and compares them to what it has heard. If there is a match and it isn't "azimuth", it appends the command list. If it is "azimuth" it recognizes its own name. Else, it returns a false command
+	def run(self):
+		"""
+		main loop
+		"""
+		self.main_loops_so_far += 1
+		if self.main_loops_so_far%10000 == 0:
+			print "main loop running 10kx" 
 		self.check_if_at_target()
-		#should probably also have it move/go, if it doesn't default to moving forward.
 
 	def set_target(self, new_targets):
 		for i in new_targets:
@@ -71,19 +79,40 @@ class FetchBot(object):
 			elif i == "azimuth":
 				print "Recognized name. Bark, bark!"
 				#this might be a good thing to map to "stop & flush targets"
+			elif i == "print":
+				print self.target_queue
+			elif i == "clear":
+				self.flush_targets()
 			else:
 				print "Not recognized target: ", i
 
 	def flush_targets(self):
 		self.target_queue = []
 
+	def run_loop(self):
+		try:
+			while True:
+				self.run()
+		except KeyboardInterrupt:
+			print "run_loop terminated"
+
+	def listen_loop(self):
+		try:
+			while True:
+				self.sr.once()
+		except KeyboardInterrupt:
+			print "listen_loop terminated"
+
+
 if __name__ == "__main__":
 	fb = FetchBot()
-	fb.sr.loop()
-	i = 0
-	while True:
-		i += 1
-		fb.mainloop()
-		if i%10000 == 0:
-			print "mainloop running."
-
+	run_thread = threading.Thread(target=fb.run_loop)
+	listen_thread = threading.Thread(target=fb.listen_loop)
+	run_thread.start()
+	rumble_thread.start()
+	try:
+		while True:
+			time.sleep(1)
+	except KeyboardInterrupt:
+		time.sleep(1)
+		exit()
