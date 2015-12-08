0e23ae2fcfc1866b4918c450271fc1681f2267e0
Matt Ruehle <matthew.t.ruehle@gmail.com>
2015-12-07 22:11:29 -0500

    loop edits

--- speech_handler.py
+++ speech_handler.py
@@ -2,7 +2,10 @@ import speech_recognition as sr
 import time
 
 
-class Speech_handler(object):
+class Background_speech_handler(object):
+	"""
+		This one is ideal, but might not be possible to implement on the RPi hardware.
+	"""
 	def __init__(self, callback_function):
 		"""
 		needs to receive a callback function.
@@ -32,3 +35,41 @@ class Speech_handler(object):
 		word_list = this_string.split(' ')
 		self.azimuth_callback(word_list)
 		return
+
+class Loop_speech_handler(object):
+	"""
+	This one might hang the processor. You've been warned.
+	TODO: Threading.
+	"""
+	def __init__(self, callback_function):
+		print "Loop handler starting..."
+		self.r = sr.Recognizer()
+		self.m = sr.Microphone(
+			# device_index = 2,
+			sample_rate = 44100, 
+			chunk_size = 8192
+			)
+		print "Adjusting for ambient noise..."
+		with self.m as source:
+			self.r.adjust_for_ambient_noise(source)
+		self.r.pause_threshold = 0.3
+		self.r.non_speaking_duration = 0
+		self.cb = callback_function
+
+	def once(self): 
+		with self.m as source:
+			print "Running once!"
+			audio = self.r.listen(source)
+		try: 
+			this_string = self.r.recognize_google(audio)
+		except sr.UnknownValueError:
+			this_string = "Couldn't understand that!"
+		except sr.RequestError:
+			print "Couldn't request results..."
+		word_list = this_string.split(' ')
+		self.cb(word_list)
+		return
+
+	def loop(self):
+		self.once()
+		self.loop()
