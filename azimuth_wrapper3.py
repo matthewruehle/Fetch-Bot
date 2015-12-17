6c0df8cf9f32183b1ad2edc27b68f26523222c6b
Matt Ruehle <matthew.t.ruehle@gmail.com>
2015-12-17 02:15:27 -0500

    Background maybe. Python3.4

--- azimuth_wrapper3.py
+++ azimuth_wrapper3.py
@@ -14,12 +14,14 @@ class FetchBot(object):
 		try:
 			self.ser = serial.Serial(serial_port, 9600, timeout=2)
 		except:
-			self.ser = serial.Serial("/dev/ttyACM0", 9600, timeout=2)
+			try:
+				self.ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=2)
+			except:
+				self.ser = serial.Serial("/dev/ttyACM0", 9600, timeout=2)
 		self.target_queue = []
 		self.target_queue = ["1", "salt", "2", "pepper", "3"] # setting at start, for testing purposes.
 		self.hasObject = False
 		self.words_to_targets_dict = {"one": "1", "1": "1", "won": "1", "2":"2", "3":"3", "two": "2", "to": "2", "too": "2", "three": "3", "tree": "3", "salt": "salt", "alt": "salt", "pepper": "pepper", "rapper":"pepper"} # Do we want these strings as the values encoded by the QR codes? Or are we going for coordinates?
-		self.sr = speech_handler.Loop_speech_handler(self.set_target)
 
 	def check_if_at_target(self):
 		if len(self.target_queue) == 0:
@@ -116,13 +118,18 @@ class FetchBot(object):
 
 	def listen_loop(self):
 		try:
-			while True:
-				self.sr.once()
-		# except KeyboardInterrupt:
-			# print "listen_loop terminated"
+			self.sr = speech_handler.Background_speech_handler(self.set_target)
+
+		# 	while True:
+		# 		self.sr.once()
+		# # except KeyboardInterrupt:
+		# 	# print "listen_loop terminated"
 		except IOError:
 			print("Got ioerror; resetting speech handler.")
-			self.sr = speech_handler.Loop_speech_handler(self.set_target)
+			
+			# self.sr = speech_handler.Background_speech_handler(self.set_target)
+
+			# self.sr = speech_handler.Loop_speech_handler(self.set_target)
 			#Not sure if this actually fixes the problem we're getting - persistent, hard-to-figure-out-why IOError keeps showing up occasionally.
 			#Specifically, IOError: stream closed. Refreshing the microphone might reopen the stream?
 			#Maybe find some way to pre-emptively "save" the audio? Not sure if that's possible.
